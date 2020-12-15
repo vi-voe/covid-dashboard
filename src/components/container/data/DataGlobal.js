f0eed058baaf0f6923211c723926979d2177ed5f
Alexander Snitko <pennywisexy@gmail.com>
2020-12-15 15:05:48 +0300

    feat: made global data component

---
+++ src/components/container/data/DataGlobal.js
@@ -0,0 +1,34 @@
+class Data {
+  constructor() {
+  }
+  static async getData(value) {
+    let url = 'https://api.covid19api.com/summary';
+    let response = await fetch(url);
+    let resultData = await response.json();
+    return await resultData[value]
+
+  }
+  static async getPromiseValue(mainStatObj, objValue) {
+    let promise = await Promise.resolve(Data.getData(mainStatObj));
+    return await promise[objValue]
+  }
+  async globalCount() {
+    return await Data.getPromiseValue('Global', 'TotalConfirmed')
+  }
+  async totalDeaths() {
+    return await Data.getPromiseValue('Global', 'TotalDeaths')
+  }
+  async totalRecovered() {
+    return await Data.getPromiseValue('Global', 'TotalRecovered')
+  }
+  async globalCountBy100K() {
+    return await Data.getPromiseValue('Global', 'TotalConfirmed').then(res => Math.round(res / 7827000000 * 100000))
+  }
+  async totalDeathsBy100K() {
+    return await Data.getPromiseValue('Global', 'TotalDeaths').then(res => Math.round(res / 7827000000 * 100000))
+  }
+  async totalRecoveredBy100K() {
+    return await Data.getPromiseValue('Global', 'TotalRecovered').then(res => Math.round(res / 7827000000 * 100000))
+  }
+}
+
