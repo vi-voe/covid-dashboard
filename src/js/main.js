9fc841c2ce3e25e2dbaa180e7016d625d0543294
ext_vvoevodkina <vivoe@yandex.ru>
2020-12-16 20:39:33 +0300

    feat: add base functionality

--- src/js/main.js
+++ src/js/main.js
@@ -9,33 +9,75 @@ container.render();
 const canvasChart = new CanvasChart();
 canvasChart.render();
 
-const config = {
-  type: 'bar',
+function generateData() {
+  return [
+    { t: 631227600000, y: '29.89' },
+    { t: 631400400000, y: '28.74' },
+    { t: 631746000000, y: '27.35' },
+    { t: 631918800000, y: '26.96' },
+    { t: 632091600000, y: '28.22' },
+    { t: 632350800000, y: '29.39' },
+    { t: 632523600000, y: '27.24' },
+    { t: 632696400000, y: '28.00' },
+    { t: 632955600000, y: '30.20' },
+  ];
+}
+
+const cfg = {
   data: {
-    labels: ['Group 1', 'Group 2'],
     datasets: [{
-      label: 'Groups',
-      data: [{
-        x: new Date(),
-        y: 1,
-      }, {
-        t: new Date(),
-        y: 10,
-      }],
+      label: 'People: ',
+      backgroundColor: '#ffe35c',
+      borderColor: '#ffe35c',
+      data: generateData(),
+      type: 'bar',
+      pointRadius: 0,
+      fill: false,
+      lineTension: 0,
+      borderWidth: 2,
     }],
-    options: {
+  },
+  options: {
+    animation: {
+      duration: 0,
+    },
+    scales: {
       xAxes: [{
         type: 'time',
-        time: {
-          unit: 'month',
+        distribution: 'series',
+        offset: true,
+        ticks: {
+          major: {
+            enabled: true,
+            fontStyle: 'bold',
+          },
+          source: 'data',
+          autoSkip: true,
+          autoSkipPadding: 25,
+          maxRotation: 0,
+          sampleSize: 100,
         },
       }],
       yAxes: [{
-        stacked: true,
-        ticks: 'data',
+        ticks: {
+          beginAtZero: true,
+        },
       }],
+    },
+    tooltips: {
+      intersect: false,
+      mode: 'x',
+      callbacks: {
+        label(tooltipItem, myData) {
+          let label = myData.datasets[tooltipItem.datasetIndex].label || '';
+          if (label) {
+            label += ': ';
+          }
+          label += parseFloat(tooltipItem.value).toFixed(2);
+          return label;
+        },
+      },
     },
   },
 };
-
-const covidChart = new Chart(canvasChart.canvas.getContext('2d'), config);
+const covidChart = new Chart(canvasChart.canvas.getContext('2d'), cfg);
