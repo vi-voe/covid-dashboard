da1d92b70ec3c4aa7c13c0975608035e22ba4f73
Alexander Snitko <pennywisexy@gmail.com>
2020-12-21 17:28:44 +0300

    feat: add geocode for markers

--- src/components/map/initMap.js
+++ src/components/map/initMap.js
@@ -2,7 +2,7 @@ function initMap() {
   const coordinates = { lat: 47.212325, lng: 38.933663 };
   const map = new google.maps.Map(document.getElementById('map'), {
     center: coordinates,
-    zoom: 8,
+    zoom: 2,
     disableDefaultUI: true,
   });
   const popupContent = '<p class="content">Что угодно</p>';
@@ -16,9 +16,30 @@ function initMap() {
   const infowindow = new google.maps.InfoWindow({
     content: popupContent,
   });
-  marker.addListener('click', () => {
+  marker.addListener('mouseover', () => {
     infowindow.open(map, marker);
   });
+
+  marker.addListener('mouseout', () => {
+    infowindow.close(map, marker);
+  });
+
+  const geo = new google.maps.Geocoder();
+  function getCountry(country) {
+    geo.geocode({ address: country }, (results, status) => {
+      if (status === google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        const marker = new google.maps.Marker({
+          map,
+          position: results[0].geometry.location,
+        });
+      }
+    });
+  }
+  console.log(geo.geocode({ address: 'USA' }));
+  getCountry('USA');
+  getCountry('Brazil');
+  getCountry('Denmark');
 
   fetch('./styleForMap.json')
     .then((response) => response.json())
